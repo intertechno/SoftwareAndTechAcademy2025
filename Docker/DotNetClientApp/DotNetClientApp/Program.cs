42527d66580de3c21d894db950ff7a68eacb8b09
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-04-22 15:41:25 +0300

    Added a C# client application for the Docker demo.

---
+++ Docker/DotNetClientApp/DotNetClientApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// make an web API call to the backend
+HttpClient client = new();
+string url = "http://localhost:3000/api";
+
+string json = client.GetStringAsync(url).Result;
+Console.WriteLine(json);
--- Docker/ExpressJsWebDemo/index.js
+++ Docker/ExpressJsWebDemo/index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/api', (req, res) => {
+  const obj = { personName: "John Doe", address: "Home St. 1234" }
+  res.json(obj);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}/`);
 });
